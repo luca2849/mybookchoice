af68b89f2afcd00bed5f00df06b363abf159eac3
Luca Passariello <luca@lucapassariello.co.uk>
2021-02-28 16:18:29 +0000

    Added basic routes structure

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json({ extend: false }));
 
 // Define Routes
-//app.use("/route", require("./routes/routes"));
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
