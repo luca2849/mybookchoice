bbe5449334344ed84f3fe77982633ef8f6015b4d
Luca Passariello <luca@lucapassariello.co.uk>
2021-03-06 16:19:56 +0000

    Added get user by username API route

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,12 +8,32 @@ const User = require("../models/User");
 
 // GET /api/user
 // Purpose - Get current user data
-// Access - Public
+// Access - Private
 router.get("/", auth, async (req, res) => {
 	try {
 		const user = await User.findOne({ _id: req.user.id }).select(
 			"-password -__v"
 		);
+		if (!user) {
+			return res
+				.status(404)
+				.json({ errors: [{ msg: "User not found" }] });
+		}
+		return res.json(user);
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ errors: [{ msg: "Internal server error" }] });
+	}
+});
+
+// GET /api/user/:username
+// Purpose - Get user data by username
+// Access - Private
+router.get("/:username", auth, async (req, res) => {
+	const { username } = req.params;
+	try {
+		const user = await User.findOne({ username }).select("-password -__v");
 		if (!user) {
 			return res
 				.status(404)
